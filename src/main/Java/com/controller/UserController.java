830be2656303c094b043d0d672094e6f6dac585d
xa <angang985@163.com>
2017-11-17 15:50:56 +0800

    job 日志 级别修改

--- src/main/Java/com/controller/UserController.java
+++ src/main/Java/com/controller/UserController.java
@@ -4,6 +4,7 @@ package com.controller;
 import com.pojo.User;
 
 import com.service_api.UserService_api;
+import com.test.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 public class UserController {
@@ -19,9 +21,12 @@ public class UserController {
     @Autowired
     private UserService_api us;
 
+    private static Logger logger = Logger.getLogger(String.valueOf(Test.class));
+
     @RequestMapping(value = "getUserInfo",method = RequestMethod.GET)
     public String getUserInfo(){
 
+        logger.info("mian:start");
         List<User> userInfos = us.getUserInfoInfo();
         System.out.println("list:"+userInfos);
         return "main";
@@ -29,6 +34,7 @@ public class UserController {
     @RequestMapping(value = "showUser",method = RequestMethod.GET)
     public String showUser(){
 
+        logger.warning("showUser:可能失败");
         List<User> userInfos = us.getUserInfoInfo();
         System.out.println("list:"+userInfos);
         return "User";
